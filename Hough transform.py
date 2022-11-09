9b18fb70d375ecd84bb346ea2e940801decfd0b8
Bruno.Iannone <iannone.1883472@studenti.uniroma1.it>
2022-11-09 23:25:33 +0100

    Working circles recognizer?

--- Hough transform.py
+++ Hough transform.py
@@ -1,46 +1,38 @@
 import sys
 import cv2 as cv
-#import google.colab.patches as cv
 import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image as im
+
+
 def main(argv):
-    #filename = 'img.png'
-    #default_file = 'smarties.png'
-    filename = argv[0]#if len(argv) > 0 else default_file
-    # Loads an image
-    src = cv.imread(filename, cv.IMREAD_COLOR)
-    # Check if image is loaded fine
-    if src is None:
-        print ('Error opening image!')
-        #print ('Usage: hough_circle.py [image_name -- default ' + default_file + '] \n')
-        return -1
-    
-    
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    
-    
-    gray = cv.medianBlur(gray, 5)
-    
-    
-    rows = gray.shape[0]
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
-                               param1=100, param2=30,
-                               minRadius=1, maxRadius=30)
-    
-    
-    if circles is not None:
+    for i in range(len(argv)):
+        print(argv[i])
+        cimg = cv.imread(argv[i], 1)
+
+        # cimg = cv.imread('circles-257002-edited.png', 1)
+        img = cv.cvtColor(cimg, cv.COLOR_RGB2GRAY)
+
+        img = cv.medianBlur(img, 5)
+        # mThres_Gray = 0.0
+        # CannyAccThresh = cv.threshold(img,mThres_Gray,0,255, cv.THRESH_OTSU)
+
+        # CannyThresh = 0.1 * CannyAccThresh
+        circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, dp=1, minDist=100,
+                                  param1=30, param2=50, minRadius=0, maxRadius=0)
+
         circles = np.uint16(np.around(circles))
+
         for i in circles[0, :]:
-            center = (i[0], i[1])
-            # circle center
-            cv.circle(src, center, 1, (0, 100, 100), 3)
-            # circle outline
-            radius = i[2]
-            cv.circle(src, center, radius, (255, 0, 255), 3)
-    
-    
-    cv.imshow("detected circles", src)
-    cv.waitKey(0)
-    
+            # draw the outer circle
+            cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+        cv.imshow("detected circles", cimg)
+        cv.waitKey(0)
+
     return 0
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
