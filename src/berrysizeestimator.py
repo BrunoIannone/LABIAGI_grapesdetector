d00b8ae8e2aa469ded26f832df0bd2d0f9b894b7
Bruno.Iannone <iannone.1883472@studenti.uniroma1.it>
2022-11-24 14:22:40 +0100

    Measurement test

--- src/berrysizeestimator.py
+++ src/berrysizeestimator.py
@@ -20,22 +20,22 @@ class BerrySizeEstimatorAbstractClass(ABC):
         self.input_image = input_image
 
     @abstractmethod
-    def Estimate(self):
+    def estimate(self):
         pass
 
     @abstractmethod
-    def Preprocessing(self):
+    def preprocessing(self):
         pass
 
     @abstractmethod
-    def BackGroundRemover(self, to_remove):
+    def back_ground_remover(self, to_remove):
         pass
 
     @abstractmethod
-    def NormalizedToImgCoordinatesForBbox(self):
+    def normalized_to_img_coordinates_for_bbox(self):
         pass
     @abstractmethod
-    def DetectReferiment(self):
+    def detect_referiment(self):
         pass
     
 
@@ -45,10 +45,10 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
     def __init__(self, input_image):
         super().__init__(input_image)
 
-    def Estimate(self):
+    def estimate(self):
         return super().estimate()
 
-    def NormalizedToImgCoordinatesForBbox(self, x_center, y_center, box_width, box_height):
+    def normalized_to_img_coordinates_for_bbox(self, x_center, y_center, box_width, box_height):
         input_width = self.input_image.shape[1]
 
         input_height = self.input_image.shape[0]
@@ -67,7 +67,7 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
 
         return (tl, tr, bl, br)
 
-    def BbVerticesDrawer(self, image, tl, tr, bl, br):  # classe test?
+    def bb_vertices_drawer(self, image, tl, tr, bl, br):  # classe test?
         dummy_input_img = image.copy()
         cv.circle(dummy_input_img, tl, 5, (0, 0, 255), -1)
         cv.circle(dummy_input_img, br, 5, (0, 0, 255), -1)
@@ -76,10 +76,10 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
 
         return dummy_input_img
 
-    def BackGroundRemover(self, to_remove):
+    def back_ground_remover(se_gf, to_remove):
         return remove(to_remove)
 
-    def DetectReferiment(self):
+    def detect_referiment(self):
         img = cv.cvtColor(self.input_image, cv.COLOR_RGB2GRAY)
         img = cv.medianBlur(img, 5)
         (T, threshInv) = cv.threshold(img, 127, 255,
@@ -94,36 +94,39 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
             circles = np.uint16(np.around(circles))
             return circles[0][0][2]
 
-    def ThresholdedCanny(self, image):
+    def thresholded_canny(self, image):
         T, threshinv = cv.threshold(
             image, 127, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         preprocessed_img = cv.Canny(threshinv, T/2, T, L2gradient=True)
+        # cv.imshow("preprocess",preprocessed_img)
+        # cv.waitKey(0)
         return preprocessed_img
 
-    def Preprocessing(self, image):
-        preprocessed_img = self.BackGroundRemover(image)
+    def preprocessing(self, image):
+        preprocessed_img = self.back_ground_remover(image)
         preprocessed_img = cv.cvtColor(preprocessed_img, cv.COLOR_BGR2GRAY)
         preprocessed_img = cv.GaussianBlur(preprocessed_img, (5, 5), 0)
+        
         return preprocessed_img
 
-    def DrawHoughResults(self,res,circles,pixelsPerMetric):
-        if (circles is not None):
-            circles = np.uint16(np.around(circles))
-            for i in circles[0, :]:
-                # draw the outer circle
-                cv.circle(res, (i[0], i[1]), i[2], (0, 255, 0), 2)
-                # draw the center of the circle
-                cv.circle(res, (i[0], i[1]), 2, (0, 0, 255), 3)
-                diameter_cm = 2*i[2] /pixelsPerMetric
-                
-                cv.putText(res, "{:.1f}cm".format(diameter_cm*pi),
-                (i[0],i[1]), cv.FONT_HERSHEY_SIMPLEX,
-                0.40, (255, 255, 255), 2)
-                print("La circonferenza è: " + str(diameter_cm *pi))
-
-                
-    def DetectorHough(self, image, res,pixelsPerMetric):
-        prepeocessed_img = self.Preprocessing(image)
+    def draw_hough_results(self,res,i,pixelsPerMetric):
+        
+        
+        # draw the outer circle
+        cv.circle(res, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv.circle(res, (i[0], i[1]), 2, (0, 0, 255), 3)
+        diameter_cm = 2*i[2] /pixelsPerMetric
+        
+        cv.putText(res, "{:.1f}cm".format(diameter_cm*pi),
+        (i[0],i[1]), cv.FONT_HERSHEY_SIMPLEX,
+        0.40, (255, 255, 255), 2)
+        print("La circonferenza è: " + str(diameter_cm *pi))
+        # cv.imshow("circle",res)
+        # cv.waitKey(0)
+        
+    def detector_hough(self, image, res,pixelsPerMetric):
+        prepeocessed_img = self.preprocessing(image)
         T, threshinv = cv.threshold(
             prepeocessed_img, 127, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         if (T == 0):
@@ -134,15 +137,20 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
                                   minDist=50, param1=T, param2=30, minRadius=0, maxRadius=0)
         # # cv.imshow("detected circles", img)
         # # cv.waitKey(0)
-        self.DrawHoughResults(res,circles,pixelsPerMetric)
-        
+        if (circles is not None):
+            circles = np.uint16(np.around(circles))
+            #print(circles[0])
+            self.draw_hough_results(res,circles[0][0],pixelsPerMetric)
+            return (circles[0][0][2] *2)/pixelsPerMetric
+        else:
+            return -1
 
     def midpoint(self,ptA, ptB): #funzione ausiliaria per il calcolo del pt medio
         return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 
     
-    def DrawResults(self,image,max_midpoint_tltr,max_midpoint_blbr,max_midpoint_tlbl,max_midpoint_trbr,maxDimA,maxDimB,max_c):
+    def draw_results(self,image,max_midpoint_tltr,max_midpoint_blbr,max_midpoint_tlbl,max_midpoint_trbr,maxDimA,maxDimB,max_box):
         #cv.drawContours(image, [max_box.astype("int")], -1, (0, 255, 0), 2)
         
         # for (x, y) in max_box:
@@ -161,17 +169,20 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
         cv.line(image, (int(max_midpoint_tlbl[0]), int(max_midpoint_tlbl[1])), (int(max_midpoint_trbr[0]), int(max_midpoint_trbr[1])),
                 (255, 0, 255), 2)
         cv.putText(image, "{:.1f}cm".format(maxDimA),
-                (int(max_midpoint_tltr[0]-100), int(
-                    max_midpoint_tltr[1]+100)), cv.FONT_HERSHEY_SIMPLEX,
+                (int(max_midpoint_tltr[0]), int(
+                    max_midpoint_tltr[1])), cv.FONT_HERSHEY_SIMPLEX,
                 0.40, (255, 255, 255), 2)
         cv.putText(image, "{:.1f}cm".format(maxDimB),
                 (int(max_midpoint_trbr[0]), int(
                     max_midpoint_trbr[1])), cv.FONT_HERSHEY_SIMPLEX,
                 0.40, (255, 255, 255), 2)
-        cv.drawContours(image,max_c,-1, (0, 255, 0), 2)
+        #print(max_box)
+        cv.drawContours(image, [max_box.astype("int")], -1, (0, 255, 0), 2)
+        # cv.imshow("res", image)
+        # cv.waitKey(0)
         return image
     def DetectorEllipses(self,cropped, image, pixelsPerMetric):
-        img_gray = self.Preprocessing(cropped)
+        img_gray = self.preprocessing(cropped)
         img_gray = self.ThresholdedCanny(img_gray)
         cnts = cv.findContours(
             image=img_gray, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_SIMPLE)
@@ -202,7 +213,7 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
             # compute the rotated bounding box of the contour
 
             ellipse = cv.fitEllipse(c)
-            print(ellipse[0])
+           
             box = cv.BoxPoints(ellipse) if imutils.is_cv2(
             ) else cv.boxPoints(ellipse)
             box = np.array(box, dtype="int")
@@ -251,7 +262,8 @@ class BerrySizeEstimator(BerrySizeEstimatorAbstractClass):
 
         if (type(max_box) != type(0)): ##non è un punto
                   
-            self.DrawResults(image,max_midpoint_tltr,max_midpoint_blbr,max_midpoint_tlbl,max_midpoint_trbr,maxDimA,maxDimB,max_c)
+            self.DrawResults(image,max_midpoint_tltr,max_midpoint_blbr,max_midpoint_tlbl,max_midpoint_trbr,maxDimA,maxDimB,max_box)
+
 
             res = pi*(3*(maxDimA+maxDimB) -
                         math.sqrt((3*maxDimA+maxDimB)*(maxDimA+3*maxDimB)))
--- src/measurement_git.py
+++ src/measurement_git.py
@@ -7,13 +7,12 @@ import glob
 #from rembg import remove
 import math
 import berrysizeestimator as est
-
+import os
 from utils_git import *
-
+ROOT_DIR = os.path.realpath(os.path.dirname(__file__))
 
 def main():
-
-    input_image = cv.imread("dw.jpeg", 1)
+    input_image = cv.imread(os.path.realpath(os.path.join(ROOT_DIR, '../dw.jpeg')), 1)
     hough_image = input_image.copy()
     ellipse_image = input_image.copy()
     # cv.imshow("input", input_image)
@@ -26,31 +25,87 @@ def main():
     # in questo caso uso la Hough transform 
     # perché sto usando un oggetto circolare
     
-    ref = estimator.DetectReferiment() 
+    ref = estimator.detect_referiment() 
     #apro il file con le coordinate della bbox
-    with open('/home/bruno/Desktop/LABIAGI_grapesdetector/yolov5/runs/detect/exp14/labels/dw.txt') as f:
+    index_line =1
+    accumulator = 0
+    counter = 0
+    min_error = 99
+    max_error = 0
+    pixel_per_metric = (2*ref)/4.0
+    with open(os.path.realpath(os.path.join(ROOT_DIR, '../dw_circle.txt'))) as f:
         line = f.readline()
     
         while(line != ''):       
-        
+            print("Acino N°: " +  str(index_line))
+            print(line)
+
         
             line = line.strip().split(" ")
-            
+            if(int(line[5]) == 0):
+                # tl, tr, bl, br = estimator.NormalizedToImgCoordinatesForBbox(
+                # float(line[1]), float(line[2]), float(line[3]), float(line[4]))
+                # temp = input_image[tl[1]:bl[1],tl[0]:br[0],: ]
+                # cv.imshow("circle",temp)
+                # cv.waitKey(0)
+
+                print("0 found")
+                line = f.readline()
+                index_line +=1
+
+                continue
+
+
             #converto le coordinate
-            tl, tr, bl, br = estimator.NormalizedToImgCoordinatesForBbox(
+            tl, tr, bl, br = estimator.normalized_to_img_coordinates_for_bbox(
                 float(line[1]), float(line[2]), float(line[3]), float(line[4]))
             
             #recupero la porzione di immagine selezionata dalla bbox
             cropped = input_image[tl[1]:bl[1],tl[0]:br[0],: ].copy()
-            
+            # cv.imshow("circle",cropped)
+            # cv.waitKey(0)
             #Per fini di test, applico lo stimatore con entrambi i test
-            #estimator.DetectorHough(cropped, hough_image[tl[1]:bl[1],tl[0]:br[0],: ],(2*ref)/4.0)
-            estimator.DetectorEllipses(cropped, ellipse_image[tl[1]:bl[1],tl[0]:br[0],: ],(2*ref)/4.0)
+            
+            estimated_diameter = estimator.detector_hough(cropped, hough_image[tl[1]:bl[1],tl[0]:br[0],: ],pixel_per_metric)
+            if(estimated_diameter == -1):
+                # tl, tr, bl, br = estimator.NormalizedToImgCoordinatesForBbox(
+                # float(line[1]), float(line[2]), float(line[3]), float(line[4]))
+                # temp = input_image[tl[1]:bl[1],tl[0]:br[0],: ]
+                # cv.imshow("circle",temp)
+                # cv.waitKey(0)
+                line = f.readline()
+                index_line+=1
+                print("No valid circle")
+                continue
+            error = (float(line[6])/pixel_per_metric) - estimated_diameter
+            if(error > max_error):
+                max_error = error
+            if(error < min_error):
+                min_error = error
+                if(min_error<0):
+                    print("Negativo")
+            
+            accumulator += error
+            counter +=1
+            print("Diametro calcolato: " + str(estimated_diameter)+" cm")
+            print("Diametro effettivo : "+ str(float(line[6])/pixel_per_metric) + " cm")
+            
+            
+            
+            
+            #estimator.DetectorEllipses(cropped, ellipse_image[tl[1]:bl[1],tl[0]:br[0],: ],(2*ref)/4.0)
             
             line = f.readline()
+            index_line+=1
+            # cv.imshow("circle",hough_image[tl[1]:bl[1],tl[0]:br[0]])
+            # cv.waitKey(0)
 
-    cv.imwrite('/home/bruno/Desktop/LABIAGI_grapesdetector/resHough.jpg',hough_image)
-    cv.imwrite('/home/bruno/Desktop/LABIAGI_grapesdetector/resEllipse.jpg',ellipse_image)
+    print("Errore minimo: " + str(min_error) + " cm")
+    print("Errore massimo: " + str(max_error)+" cm")
+    print("Errore medio: " + str(accumulator/counter)+ " cm")
+    
+    cv.imwrite(os.path.realpath(os.path.join(ROOT_DIR, '../resHough.jpg')),hough_image)
+    cv.imwrite(os.path.realpath(os.path.join(ROOT_DIR, '../resEllipse.jpg')),ellipse_image)
 
 
 if __name__ == "__main__":
